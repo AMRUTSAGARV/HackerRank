e34dd75e4bfb385064714a24589be1364f2146fd
Amrut Sagar <amrutsagar06@gmail.com>
2022-08-01 19:49:07 +0530

    prime number

---
+++ primeNum.js
@@ -0,0 +1,13 @@
+const primN = (n) => {
+  if (n < 2) return `${n} is not a prime number !`;
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return `${n} is not a prime number!`;
+    } else {
+      return `${n} is a prime number !`;
+    }
+  }
+};
+
+console.log(primN(10));
